bf7535fda8e7ab99cf4d1820d7b27969c5e55df9
YukyungShim <144038906+YukyungShim@users.noreply.github.com>
2024-06-07 11:04:08 +0900

    [Bronze III] Title: 오븐 시계, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,13 @@
+A, B = map(int, input().split()) # 요리 시작 시간 (시 분)
+C = int(input()) # 요리하는 시간
+
+A += C // 60
+B += C % 60
+
+if B >= 60:
+    A += 1
+    B -= 60
+if A >= 24:
+    A -= 24
+    
+print(A, B)
